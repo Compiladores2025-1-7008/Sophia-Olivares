5f735526105d12a6590ff19e8f2cb8963fdbfafa
Sarah Sophía Olivares García <112831267+SophiaOlivares@users.noreply.github.com>
2024-11-12 17:10:08 -0600

    Add files via upload

---
+++ P2/src/main/java/ClaseLexica.java
@@ -0,0 +1,7 @@
+package main.java;
+
+public enum ClaseLexica {
+    INT, FLOAT, IF, ELSE, WHILE, // Palabras clave
+    NUMERO, ID,                  // Números e identificadores
+    PYC, COMA, LPAR, RPAR        // Símbolos de puntuación
+}
